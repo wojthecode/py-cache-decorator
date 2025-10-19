134d85c133761e5562b5c797a690a6a6825e6744
Wojciech Witkowski <witus85@gmail.com>
2025-10-19 16:29:30 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,6 +1,29 @@
+from functools import wraps
 from typing import Callable
 
 
+def is_inmutable(*arg) -> bool:
+    return isinstance(arg, (int, float, complex, str, tuple, frozenset, bytes))
+    """Check if argument is inmutable."""
+
+
 def cache(func: Callable) -> Callable:
-    # Write your code here
-    pass
+    store = {}  # Contains cached results
+
+    @wraps(func)
+    def inner(*args, **kwargs) -> None:
+
+        if not all(is_inmutable(arg) for arg in args):
+            return func(*args, **kwargs)
+
+        key = (args, frozenset(kwargs.items()))
+
+        if key in store:
+            print("Getting from cache")
+            return store.get(key)
+        else:
+            print("Calculating new result")
+            result = func(*args, **kwargs)
+            store[key] = result
+            return result
+    return inner
